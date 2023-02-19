1a652f3c4c7852c1e8b022c347eab83b99997574
madyanes <madyanes.1995@gmail.com>
2023-02-19 11:24:36 +0700

    add process_menu and run methods

--- design/menu_builder.py
+++ design/menu_builder.py
@@ -15,7 +15,8 @@ class MenuBuilder(object):
     def register(self, key: str, value: str, fn=None):
         new_menu = {
             'key': str(key),
-            'value': str(value)
+            'value': str(value),
+            'fn': fn
         }
         self.menu.append(new_menu)
 
@@ -25,15 +26,44 @@ class MenuBuilder(object):
             print(menu_item['key'] + '/', menu_item['value'])
         print(self.wrapper.content_center(self.wrap_char))
 
+    def process_menu(self, selected_menu):
+        for menu_item in self.menu:
+            if selected_menu == menu_item['key']:
+                print(menu_item['fn']())
+                print(self.wrapper.content_left_right('_', '(Press enter to continue.)', True))
+                input()
+                break
+
+    def run(self):
+        while True:
+            self.get_menu()
+            print()
+            selected_menu = input('Select a menu: ')
+            self.process_menu(selected_menu)
+
+
+def tell_your_name():
+    name = input('My name is: ')
+    return f'Hello, my name is {name}'
+
+
+def solve_math():
+    print(f'How much 5 + 5?')
+    result = 5 + 5
+    answer = int(input())
+    if answer != result:
+        return False
+    return True
+
 
 if __name__ == '__main__':
-    menu = MenuBuilder('Available Menu'.upper())
-    menu.register('a', 'Tell me your name!')
-    menu.register('b', 'Where do you live?')
-    menu.get_menu()
+    menu = MenuBuilder('Available Menu'.upper(), 100)
+    menu.register('a', 'Tell me your name!', tell_your_name)
+    menu.register('b', 'Can you solve this math?', solve_math)
+    menu.run()
 
     # output
-    # ==================AVAILABLE MENU==================
+    # ===========================================AVAILABLE MENU===========================================
     # a/ Tell me your name!
     # b/ Where do you live?
-    # ==================================================
+    # ====================================================================================================
