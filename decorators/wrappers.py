81b26472130d97824fdfc4a77df5dd4e91624f1d
madyanes <madyanes.1995@gmail.com>
2023-02-19 07:48:54 +0700

    add content_left_right method

--- decorators/wrappers.py
+++ decorators/wrappers.py
@@ -13,11 +13,25 @@ class Wrapper(object):
             content = str(filled_characters)
         return content.center(max_length, filled_characters)
 
+    def content_left_right(self, max_length: int, filled_characters: str = '-', content: str = '', start_from_right: bool = False) -> str:
+        content_length: int = len(content)
+        filled_characters_length_needed = max_length - content_length
+        calculated_filled_characters = filled_characters * filled_characters_length_needed
+        if start_from_right:
+            return calculated_filled_characters + content
+        return content + calculated_filled_characters
+
 
 if __name__ == '__main__':
     wrapper = Wrapper()
-    result = wrapper.content_center(100, '_')
+    result = wrapper.content_left_right(70, '_', 'I ❤ Python')
+    print(result)
+    result = wrapper.content_center(70, '_', 'I ❤ Python')
+    print(result)
+    result = wrapper.content_left_right(70, '_', 'I ❤ Python', True)
     print(result)
 
     # output
-    # ____________________________________________________________________________________________________
+    # I ❤ Python____________________________________________________________
+    # ______________________________I ❤ Python______________________________
+    # ____________________________________________________________I ❤ Python
